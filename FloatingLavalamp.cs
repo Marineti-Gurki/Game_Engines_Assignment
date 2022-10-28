07e4861c0cb9573a3f3f2b62b535e6054961e380
matissolus <matiss2000@inbox.lv>
2022-10-29 00:20:06 +0100

    LavaLamp
    Changed to most things to physicsprocess

--- FloatingLavalamp.cs
+++ FloatingLavalamp.cs
@@ -14,7 +14,7 @@ public class FloatingLavalamp : MeshInstance
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
+    public override void _PhysicsProcess(float delta)
     {
         Theta = Time.GetTicksMsec() * (Frequency * TimeScale * delta);
         float Angle = Amplitude * Mathf.Sin(Theta);
