5142079b4df7db38344f581594f280a653649ac0
matissolus <matiss2000@inbox.lv>
2022-10-29 23:15:34 +0100

    Shader improvements and added for loop for generating a lot of lamps

--- MainGame.cs
+++ MainGame.cs
@@ -3,14 +3,27 @@ using System;
 
 public class MainGame : Spatial
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
+    PackedScene Lavalamp;
+    bool made = false;
+    int loops = 10;
     public override void _Ready()
     {
-
+        Lavalamp = (PackedScene)ResourceLoader.Load("res://LavaLamp.tscn");
+        int radius = 5;
+        for (int i = 0; i < loops; i++)
+        {
+            float numOfScenes = (float)(2.0f * Mathf.Pi * i);
+            float theta = Mathf.Pi * (2.0f) / (numOfScenes);
+            for (int j = 0; j < numOfScenes; j++)
+            {
+                float angle = j * theta;
+                float x = (radius) * Mathf.Cos(angle) * i;
+                float y = (radius) * Mathf.Sin(angle) * i;
+                MeshInstance LLMPInst = Lavalamp.Instance<MeshInstance>();
+                LLMPInst.Translation = new Vector3(x, y, 0);
+                AddChild(LLMPInst);
+            }
+        }
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
